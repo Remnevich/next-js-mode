2d9673638ce9642929ed307d72d4b99021a80a56
Vladislav Remnev <remnevvlad30@gmail.com>
2025-04-08 11:24:47 +0300

    Подключение базы данных с помощью Neon + настройка через drizzle

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-// export const revalidate = 5 ревалидация для продакшн билда каждые 5 секунд если есть какой то запрос
-// export const dynamic = "force-dynamic" ревалидация для продакшн билда при каждом обновлении страницы
+import {Button} from "@/components/ui/button";
 
-export default function Home() {
-    // cookies() ревалидация для продакшн билда при каждом обновлении страницы
+export default async function Home() {
     return (
         <div>
-            {Date.now()}
+            <Button variant={'default'} size={'sm'}>Click here</Button>
         </div>
     );
 }
